e7d17eaac7ae1814a6fea211c43fbca71143825c
orwellian225 <orwellian225@gmail.com>
2024-08-22 23:56:39 +0200

    add: unfinished graph on heap and subgraph search

--- include/graph.h
+++ include/graph.h
@@ -7,9 +7,13 @@ struct Graph {
     Matrix matrix;
 
     static Graph unfinished(size_t n);
+    static Graph *heap_unfinished(size_t n);
+
     static Graph complete(size_t n);
     static Graph empty(size_t n);
 
     void print() const;
     bool has_subgraph(const Graph& H);
+
+    Graph *clone();
 };
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,6 +1,8 @@
 #include <stack>
 #include <vector>
 
+#include <fmt/core.h>
+
 #include "graph.h"
 
 Graph Graph::unfinished(size_t n) {
@@ -12,6 +14,23 @@ Graph Graph::unfinished(size_t n) {
                 continue;
 
             g.matrix[{i,j}] = -1;
+        }
+    }
+    
+    return g;
+}
+
+Graph *Graph::heap_unfinished(size_t n) {
+    Graph *g = new Graph; 
+    g->order = n;
+    g->matrix = Matrix::new_uniform(n, 0);
+
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = 0; j < n; ++j) {
+            if (i == j)
+                continue;
+
+            g->matrix[{i,j}] = -1;
         }
     }
     
@@ -43,16 +62,58 @@ void Graph::print() const {
 }
 
 bool Graph::has_subgraph(const Graph& H) {
-    std::stack<std::vector<size_t>> stack;
-    stack.push(std::vector<size_t>());
+    std::stack<std::vector<size_t>*> stack;
+    stack.push(new std::vector<size_t>());
 
     while (!stack.empty()) {
-        auto& item = stack.top();
+        std::vector<size_t>* item = stack.top();
+        stack.pop();
 
-        if (item.size() != H.order) {
+        if (item->size() == H.order) {
+            bool local_result = true;
+            for (size_t vertex_a_in_H = 0; vertex_a_in_H < item->size(); ++vertex_a_in_H) {
+                size_t vertex_a_in_G = item->at(vertex_a_in_H);
 
+                for (size_t vertex_b_in_H = vertex_a_in_H; vertex_b_in_H < item->size(); ++vertex_b_in_H) {
+                    size_t vertex_b_in_G = item->at(vertex_b_in_H);
+
+                    if (vertex_a_in_G == vertex_b_in_G || H.matrix[{vertex_a_in_H, vertex_b_in_H}] == -1)
+                        continue;
+                    local_result = local_result && (this->matrix[{vertex_a_in_G, vertex_b_in_G}] == H.matrix[{vertex_a_in_H, vertex_b_in_H}]);
+                }
+
+            }
+
+            if (local_result) {
+                while (!stack.empty()) { // Freeing all the items that still exist
+                    std::vector<size_t>* temp = stack.top();
+                    stack.pop();
+                    delete temp;
+                }
+                return true;
+            }
         } else {
-            
+            for (size_t i = (item->size() > 0 ? item->at(item->size() - 1) + 1 : 0); i < this->order; ++i) {
+                std::vector<size_t>* new_item = new std::vector<size_t>(*item);
+                new_item->push_back(i);
+                stack.push(new_item);
+            }
+
+            delete item;
         }
     }
+
+    return false;
+}
+
+Graph *Graph::clone() {
+    Graph *new_graph = new Graph;
+    new_graph->order = this->order;
+    new_graph->matrix = Matrix::new_uniform(this->order, 0);
+
+    for (size_t i = 0; i < this->order; ++i)
+        for (size_t j = 0; j < this->order; ++j)
+            new_graph->matrix[{i, j}] = this->matrix[{i, j}];
+
+    return new_graph;
 }
